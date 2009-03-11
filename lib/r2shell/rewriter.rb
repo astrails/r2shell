6290029aeb74512c12064ed61ec613ea6598eed6
Vitaly Kushner <vitaly-at-astrails-dot-com>
2009-03-11 19:15:54 +0200

    clear expressions after processing

--- lib/r2shell/rewriter.rb
+++ lib/r2shell/rewriter.rb
@@ -2,35 +2,44 @@
 module R2shell
   class Rewriter < SexpProcessor
 
-
-
     def process(exp)
-      puts "EXP:\t#{exp.inspect}" if $TEST
+      puts "PROCESS:\t#{exp.inspect}" if $TEST
 
       return exp unless exp.is_a?(Array)
       
-      case exp.first
+      result = case exp.first
       when :block
         # for block all the values except for the last are lost, so we need to exec them
         last = exp.pop
         res = exp.map{|s| process2(s)}
         res << process(last)
+        exp.clear
+        res
       when :iter, :for
         last = exp.pop
         exp.map{|s| process(s)} << process2(last)
       when :if
         res = [exp.shift]
         res << process2(exp.shift, false)
-        res + exp.map {|s| process(s)}
+        res = res + exp.map {|s| process(s)}
+        exp.clear
+        res
+      when :or, :and, :not
+        res = [exp.shift]
+        res = res + exp.map {|s| process2(s, false)}
+        exp.clear
+        res
       else
         super
       end
+      puts "RES: #{result.inspect}" if $TEST
+      result
     end
     
     def process2(exp, check_errors = true)
       return exp unless exp.is_a?(Array)
       
-      puts "EXP2:\t#{exp.inspect}" if $TEST
+      puts "PROCESS2:\t#{exp.inspect}" if $TEST
       
       autoexec(process(exp), check_errors)
     end
